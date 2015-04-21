f7ee306fe109ef3e3f25c9f4b7eb1cac67413c2a
Andreas Bresser <a.bresser@greyrook.com>
2015-04-21 12:54:38 +0200

    validate minimum/maximum when set

--- src/controls/Slider.js
+++ src/controls/Slider.js
@@ -5,8 +5,8 @@
 PIXI_UI.Slider = function(theme) {
     this.skinName = this.skinName || PIXI_UI.Slider.SKIN_NAME;
 
-    this.minimum = this.minimum || 0;
-    this.maximum = this.maximum || 100;
+    this._minimum = this._minimum || 0;
+    this._maximum = this._maximum || 100;
     this.step = this.step || 0; //TODO: implement me!
     this.page = this.page || 10;
     this._value = this.minimum;
@@ -29,10 +29,18 @@ PIXI_UI.Slider.prototype.thumbMoved = function(x, y) {
     var max = 1, value = 0;
     if (this.orientation === PIXI_UI.Scrollable.HORIZONTAL) {
         max = this.width - this.thumb.width;
-        value = x;
+        if (this._inverse) {
+            value = max - x;
+        } else {
+            value = x;
+        }
     } else {
         max = this.height - this.thumb.height;
-        value = y;
+        if (this._inverse) {
+            value = max - y;
+        } else {
+            value = y;
+        }
     }
     value = value / max * (this.maximum - this.minimum) + this.minimum;
     this.value = value;
@@ -54,6 +62,40 @@ Object.defineProperty(PIXI_UI.Slider.prototype, 'value', {
             sliderData.value = this._value;
             sliderData.target = this;
             this.change(sliderData);
+        }
+    }
+});
+
+/**
+ * set minimum and update value if necessary
+ */
+Object.defineProperty(PIXI_UI.Slider.prototype, 'minimum', {
+    get: function() {
+        return this._minimum;
+    },
+    set: function(minimum) {
+        if(!isNaN(minimum) && this.minimum !== minimum && minimum < this.maximum) {
+            this._minimum = minimum;
+        }
+        if (this._value < this.minimum) {
+            this.value = this._value;
+        }
+    }
+});
+
+/**
+ * set maximum and update value if necessary
+ */
+Object.defineProperty(PIXI_UI.Slider.prototype, 'maximum', {
+    get: function() {
+        return this._maximum;
+    },
+    set: function(maximum) {
+        if(!isNaN(maximum) && this.maximum !== maximum && maximum > this.minimum) {
+            this._maximum = maximum;
+        }
+        if (this._value > this.maximum) {
+            this.value = maximum;
         }
     }
 });
