7f8da475e48da17656e281494fcf8a71418b4825
Andreas Bresser <a.bresser@greyrook.com>
2015-05-16 23:53:21 +0200

    Bugfix: use named functions that can be easily referenced for positioning

--- src/util/position.js
+++ src/util/position.js
@@ -1,44 +1,51 @@
+/**
+ * center element on parent vertically
+ * @param elem
+ * @param parent (optional)
+ * @method centerVertical
+ */
+function centerVertical(elem, parent) {
+    parent = parent || elem.parent;
+    elem.y = Math.floor((parent.height - elem.height ) / 2);
+}
+
+/**
+ *
+ * @param elem
+ * @param parent (optional)
+ */
+function bottom(elem, parent) {
+    parent = parent || elem.parent;
+    elem.y = parent.y - elem.height;
+}
+
+/**
+ * center element on parent horizontally
+ * @param elem
+ * @param parent (optional)
+ * @method centerHorizontal
+ */
+function centerHorizontal(elem, parent) {
+    parent = parent || elem.parent;
+    elem.x = Math.floor((parent.width - elem.width ) / 2);
+}
+
+
+/**
+ * center element on parent
+ * @param elem
+ * @param parent (optional)
+ * @method center
+ */
+function center(elem, parent) {
+    centerVertical(elem, parent);
+    centerHorizontal(elem, parent);
+}
+
+
 module.exports = {
-    /**
-     * center element on parent horizontally
-     * @param elem
-     * @param parent (optional)
-     * @method centerHorizontal
-     */
-    centerHorizontal: function (elem, parent) {
-        parent = parent || elem.parent;
-        elem.x = Math.floor((parent.width - elem.width ) / 2);
-    },
-
-    /**
-     * center element on parent vertically
-     * @param elem
-     * @param parent (optional)
-     * @method centerVertical
-     */
-    centerVertical: function (elem, parent) {
-        parent = parent || elem.parent;
-        elem.y = Math.floor((parent.height - elem.height ) / 2);
-    },
-
-    /**
-     * center element on parent
-     * @param elem
-     * @param parent (optional)
-     * @method center
-     */
-    center: function (elem, parent) {
-        PIXI_UI.centerVertical(elem, parent);
-        PIXI_UI.centerHorizontal(elem, parent);
-    },
-
-    /**
-     *
-     * @param elem
-     * @param parent (optional)
-     */
-    bottom: function (elem, parent) {
-        parent = parent || elem.parent;
-        elem.y = parent.y - elem.height;
-    }
+    centerHorizontal: centerHorizontal,
+    centerVertical: centerVertical,
+    center: center,
+    bottom: bottom
 };
