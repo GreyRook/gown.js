e4c493eba5c145f32882d6e8a00995396fa953b1
Andreas Bresser <a.bresser@greyrook.com>
2015-05-16 23:10:53 +0200

    Bugfix: correct use of module.export for positioning functions

--- src/util/position.js
+++ src/util/position.js
@@ -1,42 +1,44 @@
-/**
- * center element on parent horizontally
- * @param elem
- * @param parent (optional)
- * @method centerHorizontal
- */
-PIXI_UI.centerHorizontal = function(elem, parent) {
-    parent = parent || elem.parent;
-    elem.x = Math.floor((parent.width - elem.width ) / 2);
-};
+module.exports = {
+    /**
+     * center element on parent horizontally
+     * @param elem
+     * @param parent (optional)
+     * @method centerHorizontal
+     */
+    centerHorizontal: function (elem, parent) {
+        parent = parent || elem.parent;
+        elem.x = Math.floor((parent.width - elem.width ) / 2);
+    },
 
-/**
- * center element on parent vertically
- * @param elem
- * @param parent (optional)
- * @method centerVertical
- */
-PIXI_UI.centerVertical = function(elem, parent) {
-    parent = parent || elem.parent;
-    elem.y = Math.floor((parent.height - elem.height ) / 2);
-};
+    /**
+     * center element on parent vertically
+     * @param elem
+     * @param parent (optional)
+     * @method centerVertical
+     */
+    centerVertical: function (elem, parent) {
+        parent = parent || elem.parent;
+        elem.y = Math.floor((parent.height - elem.height ) / 2);
+    },
 
-/**
- * center element on parent
- * @param elem
- * @param parent (optional)
- * @method center
- */
-PIXI_UI.center = function(elem, parent) {
-    PIXI_UI.centerVertical(elem, parent);
-    PIXI_UI.centerHorizontal(elem, parent);
-};
+    /**
+     * center element on parent
+     * @param elem
+     * @param parent (optional)
+     * @method center
+     */
+    center: function (elem, parent) {
+        PIXI_UI.centerVertical(elem, parent);
+        PIXI_UI.centerHorizontal(elem, parent);
+    },
 
-/**
- *
- * @param elem
- * @param parent (optional)
- */
-PIXI_UI.bottom = function(elem, parent) {
-    parent = parent || elem.parent;
-    elem.y = parent.y - elem.height;
+    /**
+     *
+     * @param elem
+     * @param parent (optional)
+     */
+    bottom: function (elem, parent) {
+        parent = parent || elem.parent;
+        elem.y = parent.y - elem.height;
+    }
 };
