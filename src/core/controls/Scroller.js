b70780750d4f407159b955c356ddf579d5c7fa45
Andreas Bresser <a.bresser@greyrook.com>
2015-08-14 17:46:33 +0200

    Scroller (like Starling/Feathers Scroller - WIP)

---
+++ src/core/controls/Scroller.js
@@ -0,0 +1,38 @@
+var Control = require('../Control'),
+    LayoutAlignment = require('../layout/LayoutAlignment');
+
+/**
+ * The Scroller hosts some content that can be scrolled. The width/height
+ * of the Scroller defines the viewport.
+ *
+ * @class Scroller
+ * @extends PIXI_UI.Control
+ * @memberof PIXI_UI
+ * @constructor
+ */
+function Scroller() {
+    Control.call(this);
+}
+
+Scroller.prototype = Object.create( Control.prototype );
+Scroller.prototype.constructor = Scroller;
+module.exports = Scroller;
+
+/**
+ * Creates and adds the <code>horizontalScrollBar</code> and
+ * <code>verticalScrollBar</code> sub-components and removes the old
+ * instances, if they exist.
+ *
+ * <p>Meant for internal use, and subclasses may override this function
+ * with a custom implementation.</p>
+ *
+ * @see #horizontalScrollBar
+ * @see #verticalScrollBar
+ * @see #horizontalScrollBarFactory
+ * @see #verticalScrollBarFactory
+ */
+Scroller.prototype.createScrollBars = function() {
+
+}
+
+// TODO: scrollSteps pageIndex updateVerticalScrollFromTouchPosition throwTo hideHorizontalScrollBar revealHorizontalScrollBar
