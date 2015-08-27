6c6f54ca879e0b6177d30d215dfdd45fba655662
Julian Laubstein <j.laubstein@greyrook.com>
2015-08-27 14:53:11 +0200

    Fixed another rename failure

--- src/core/index.js
+++ src/core/index.js
@@ -1,12 +1,12 @@
 /**
- * @file        Main export of the GOWN core library
+ * @file        Main export of the gown.js core library
  * @author      Andreas Bresser <andreasbresser@gmail.com>
  * @copyright   2015 Andreas Bresser
  * @license     {@link https://github.com/brean/gown.js/blob/master/LICENSE|Apache License}
  */
 
 /**
- * @namespace PIXI.core
+ * @namespace GOWN.core
  */
 module.exports = {
     Control:        require('./Control'),
