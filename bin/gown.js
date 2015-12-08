47841bda33abce55601c4f9e24d97ae57636540f
Andreas Bresser <a.bresser@greyrook.com>
2015-12-08 12:17:27 +0100

    special case for mixin

--- bin/gown.js
+++ bin/gown.js
@@ -4870,8 +4870,14 @@ module.exports = function(destination, source) {
     for (var key in source) {
         if (source.hasOwnProperty(key)) {
             if(key === 'defineProperty') {
-                for(var name in source[key]) {
-                    Object.defineProperty(destination, name, source[key][name]);
+                for (var name in source[key]) {
+                    var data = source[key][name];
+                    if (data.configurable === undefined) {
+                         // We change our default case, so that we can
+                         // overwrite properties later on
+                        data.configurable = true;
+                    }
+                    Object.defineProperty(destination, name, data);
                 }
             } else {
                 destination[key] = source[key];
