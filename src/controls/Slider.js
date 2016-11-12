3cd94d95a14fd4da55679f4dec4786c1237732da
Andreas Bresser <andreasbresser@gmail.com>
2016-11-12 22:00:27 +0100

    Fix constructor parameter

--- src/controls/Slider.js
+++ src/controls/Slider.js
@@ -8,39 +8,15 @@ var Scrollable = require('./Scrollable');
  * @memberof GOWN
  * @constructor
  */
+// TODO: move stuff from Scrollable back here?
+function Slider(theme) {
+    this._skinName = this._skinName || Slider.SKIN_NAME;
 
-function Slider(thumb, theme, skinName) {
-    this.skinName = skinName || Slider.SKIN_NAME;
-
-    this._minimum = this._minimum || 0;
-    this._maximum = this._maximum || 100;
-    this.step = this.step || 0; //TODO: implement me!
-    this.page = this.page || 10; //TODO: implement me!
-    this._value = this.minimum;
-    this.change = null;
-
-    Scrollable.call(this, thumb, theme);
+    Scrollable.call(this, theme);
 }
 
 Slider.prototype = Object.create( Scrollable.prototype );
 Slider.prototype.constructor = Slider;
 module.exports = Slider;
 
-
 Slider.SKIN_NAME = 'scroll_bar';
-
-/**
- * thumb has been moved - calculate new value
- *
- * @param x x-position to scroll to (ignored when vertical)
- * @param y y-position to scroll to (ignored when horizontal)
- */
-Slider.prototype.thumbMoved = function(x, y) {
-    var pos = 0;
-    if (this.direction === Scrollable.HORIZONTAL) {
-        pos = x;
-    } else {
-        pos = y;
-    }
-    this.value = this.pixelToValue(pos);
-};
