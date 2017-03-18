6f240263e8e0772787eb1f862ba9f044da7b56a2
Andreas Bresser <andreasbresser@gmail.com>
2017-03-18 19:40:39 +0100

    change links to current github repo

--- src/core/index.js
+++ src/core/index.js
@@ -1,8 +1,8 @@
 /**
  * @file        Main export of the gown.js core library
  * @author      Andreas Bresser <andreasbresser@gmail.com>
- * @copyright   2015 Andreas Bresser
- * @license     {@link https://github.com/brean/gown.js/blob/master/LICENSE|Apache License}
+ * @copyright   2017 Andreas Bresser
+ * @license     {@link https://github.com/GreyRook/gown.js/blob/master/LICENSE|Apache License}
  */
 
 /**
