bf634331d5e3b8e744f2cd3076abab06ef1fd9f2
vdbongard <daniel@vdbongard.com>
2017-04-05 11:23:18 +0200

    Fixed setting maximum number of characters in InputControl

--- src/controls/InputControl.js
+++ src/controls/InputControl.js
@@ -288,6 +288,9 @@ Object.defineProperty(InputControl.prototype, 'text', {
     },
     set: function (text) {
         text += ''; // add '' to assure text is parsed as string
+
+        if(this.maxChars > 0 && text.length > this.maxChars) return;
+
         if (this._origText === text) {
             // return if text has not changed
             return;
@@ -329,6 +332,7 @@ Object.defineProperty(InputControl.prototype, 'maxChars', {
             );
         }
         this._maxChars = value;
+        InputWrapper.setMaxLength(value);
 
     }
 });
