25b9459cd6af5c407948b0fc4ef6c9e72d651968
vdbongard <daniel@vdbongard.com>
2017-06-21 15:22:25 +0200

    Fix: List documentation

--- src/controls/List.js
+++ src/controls/List.js
@@ -7,50 +7,113 @@ var DefaultListItemRenderer = require('./renderers/DefaultListItemRenderer');
  * The basic list
  *
  * @class List
- * @extends GOWN.List
+ * @extends GOWN.Scroller
  * @memberof GOWN
  * @constructor
+ * @param [theme] theme for the list {Theme}
  */
 function List(theme) {
     Scroller.call(this, theme);
-    this.skinName = this.skinName || List.SKIN_NAME;
-
-    // Determines if items in the list may be selected.
-    this._selectable = true;
-
-    // The index of the currently selected item.
-    this._selectedIndex = -1;
-
-    // If true multiple items may be selected at a time.
-    this._allowMultipleSelection = false;
-
-    // The indices of the currently selected items.
-    this._selectedIndices = [];
-
-    this._itemRenderer = [];
-    this._itemChangeHandler = this.itemChangeHandler.bind(this);
-    this._itemRendererChangeHandler = this.itemRendererChangeHandler.bind(this);
-
-    // create new instance of the item renderer
-    this._itemRendererFactory = this._itemRendererFactory || this._defaultItemRendererFactory;
-
-    // The collection of data displayed by the list.
 
     /**
-     * properties that will be passed down to every item
-     * renderer when the list validates.
+     * The skin name
+     *
+     * @type String
+     * @default List.SKIN_NAME
+     */
+    this.skinName = this.skinName || List.SKIN_NAME;
+
+    /**
+     * Determines if items in the list may be selected. (not implemented yet)
+     *
+     * @private
+     * @type bool
+     * @default true
+     */
+    this._selectable = true;
+
+    /**
+     * The index of the currently selected item.
+     *
+     * @private
+     * @type Number
+     * @default -1
+     */
+    this._selectedIndex = -1;
+
+    /**
+     * If true multiple items may be selected at a time.
+     *
+     * @private
+     * @type bool
+     * @default false
+     */
+    this._allowMultipleSelection = false;
+
+    /**
+     * The indices of the currently selected items.
+     *
+     * @private
+     * @type Number[]
+     * @default []
+     */
+    this._selectedIndices = [];
+
+    /**
+     * The item renderer
+     *
+     * @private
+     * @type Array
+     * @default []
+     */
+    this._itemRenderer = [];
+
+    /**
+     * The item change handler
+     *
+     * @private
+     * @type function
+     */
+    this._itemChangeHandler = this.itemChangeHandler.bind(this);
+
+    /**
+     * The item renderer change handler
+     *
+     * @private
+     * @type function
+     */
+    this._itemRendererChangeHandler = this.itemRendererChangeHandler.bind(this);
+
+    /**
+     * The item renderer factory creates a new instance of the item renderer
+     *
+     * @private
+     * @type function
+     * @default this._defaultItemRendererFactory
+     */
+    this._itemRendererFactory = this._itemRendererFactory || this._defaultItemRendererFactory;
+
+    /**
+     * Properties that will be passed down to every item renderer when the list validates.
+     *
+     * @private
+     * @type Object
+     * @default {}
      */
     this._itemRendererProperties = {};
 
     // TODO: itemRendererStyleName (?)
 
-    // initialize
     if (!this.viewPort) {
-
-        // We do not implement ListDataViewPort from feathers
-        // (most of what it does is implemented in List directly to
-        //  manage the viewport)
-        // and instead use the normal LayoutGroup (less abstraction, less code)
+        /**
+         * We do not implement ListDataViewPort from feathers
+         * (most of what it does is implemented in List directly to
+         * manage the viewport)
+         * and instead use the normal LayoutGroup (less abstraction, less code)
+         *
+         * @private
+         * @type GOWN.LayoutGroup
+         */
         this.viewPort = new LayoutGroup();
     }
     this.layoutChanged = true;
@@ -60,25 +123,45 @@ List.prototype = Object.create( Scroller.prototype );
 List.prototype.constructor = List;
 module.exports = List;
 
-// name of skin that will be applied
+/**
+ * Default list skin name
+ *
+ * @static
+ * @final
+ * @type String
+ */
 List.SKIN_NAME = 'list';
 
 /**
  * Dispatched when the selected item changes.
+ *
+ * @static
+ * @final
+ * @type String
  */
 List.CHANGE = 'change';
 
+/**
+ */
 
 /**
- * A function called that is expected to return a new item renderer
+ * A function that is expected to return a new item renderer*
+ *
+ * @param theme The item theme {Theme}
+ * @returns {DefaultListItemRenderer}
+ * @private
  */
 List.prototype._defaultItemRendererFactory = function(theme) {
     return new DefaultListItemRenderer(theme);
 };
 
+/**
+ * Gets called when new data is added or removed
+ * to the dataProvider
+ *
+ * @protected
+ */
 List.prototype.itemChangeHandler = function() {
-    // this code is executed when new data is added or removed
-    // to the dataProvider
     // TODO: test code so it will handle if item is removed
     // deselect removed items
     var index = this._dataProvider.data.length;
@@ -97,19 +180,25 @@ List.prototype.itemChangeHandler = function() {
 };
 
 /**
- * select one of the item
+ * Select one of the items
+ *
+ * @param item The item to select {String}
  */
 List.prototype.selectItem = function(item) {
     this.selectedIndex = this._dataProvider.data.indexOf(item);
 };
 
 
+/**
+ * @private
+ */
 // performance increase to avoid using call.. (10x faster)
 List.prototype.scrollerRedraw = Scroller.prototype.redraw;
-/**
- * update before draw call
- *
 
+/**
+ * Update before draw call
+ *
+ * @protected
  */
 List.prototype.redraw = function() {
     var basicsInvalid = this.dataInvalid;
@@ -128,6 +217,9 @@ List.prototype.redraw = function() {
     }
 };
 
+/**
+ * Refresh the renderers
+ */
 List.prototype.refreshRenderers = function () {
     //TODO: update only new renderer
     //      see ListDataViewPort --> refreshInactieRenderers
@@ -153,9 +245,11 @@ List.prototype.refreshRenderers = function () {
 };
 
 /**
- * item catch/forward renderer change event
- * this is thrown when the state of the itemRenderer Changes
+ * Item catch/forward renderer change event.
+ * This is thrown when the state of the itemRenderer changes
  * (e.g. from unselected to selected), not when the data changes
+ *
+ * @protected
  */
 List.prototype.itemRendererChangeHandler = function(itemRenderer, value) {
     // TODO: update selected item
@@ -183,9 +277,9 @@ List.prototype.itemRendererChangeHandler = function(itemRenderer, value) {
 };
 
 /**
- * set layout and pass eventlistener to it
+ * Set layout and pass event listener to it
  *
- * @property layout
+ * @name GOWN.List#layout
  * @type LayoutAlignment
  */
 Object.defineProperty(List.prototype, 'layout', {
@@ -207,10 +301,10 @@ Object.defineProperty(List.prototype, 'layout', {
 });
 
 /**
- * set item renderer properties (e.g. labelField) and update all itemRenderer
+ * Set item renderer properties (e.g. labelField) and update all itemRenderer
  *
- * @property itemRendererProperties
- * @type LayoutAlignment
+ * @name GOWN.List#itemRendererProperties
+ * @type Object
  */
 Object.defineProperty(List.prototype, 'itemRendererProperties', {
     set: function(itemRendererProperties) {
@@ -224,10 +318,10 @@ Object.defineProperty(List.prototype, 'itemRendererProperties', {
 
 
 /**
- * set item renderer factory (for custom item Renderer)
+ * Set item renderer factory (for custom item Renderer)
  *
- * @property itemRendererFactory
- * @type LayoutAlignment
+ * @name GOWN.List#itemRendererFactory
+ * @type function
  */
 Object.defineProperty(List.prototype, 'itemRendererFactory', {
     set: function(itemRendererFactory) {
@@ -240,11 +334,11 @@ Object.defineProperty(List.prototype, 'itemRendererFactory', {
 });
 
 /**
- * allow/disallow multiple selection
- * if selection has been disallowed, deselect all but one.
+ * Allow/disallow multiple selection.
+ * If selection has been disallowed, deselect all but one.
  *
- * @property allowMultipleSelection
- * @type Boolean
+ * @name GOWN.List#allowMultipleSelection
+ * @type bool
  */
  Object.defineProperty(List.prototype, 'allowMultipleSelection', {
      set: function(allowMultipleSelection) {
@@ -264,7 +358,12 @@ Object.defineProperty(List.prototype, 'itemRendererFactory', {
      }
  });
 
-
+/**
+ * The index of the selected item
+ *
+ * @name GOWN.List#selectedIndex
+ * @type Number
+ */
 Object.defineProperty(List.prototype, 'selectedIndex', {
     set: function(selectedIndex) {
         this._selectedIndex = selectedIndex;
@@ -277,11 +376,11 @@ Object.defineProperty(List.prototype, 'selectedIndex', {
 });
 
 /**
- * dataProvider for list
- * the dataProvider is a sturcture thats provides the data.
- * in its simplest form it is a array containing the data
+ * dataProvider for list.
+ * The dataProvider is a structure that provides the data.
+ * In its simplest form it is an array containing the data
  *
- * @property dataProvider
+ * @name GOWN.List#dataProvider
  * @type Array
  */
 Object.defineProperty(List.prototype, 'dataProvider', {
