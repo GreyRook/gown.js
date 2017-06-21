5bca6fec030838b3b721e26c8867bb25ca899f90
vdbongard <daniel@vdbongard.com>
2017-06-21 16:17:05 +0200

    Fix: Radio documentation

--- src/controls/Radio.js
+++ src/controls/Radio.js
@@ -1,13 +1,15 @@
 var ToggleButton = require('./ToggleButton');
 
 /**
-  * A toggleable control that exists in a set that requires a single, exclusive toggled item.
-  *
-  * @class Radio
-  * @extends GOWN.ToggleButton
-  * @memberof GOWN
-  * @constructor
-  */
+ * A toggleable control that exists in a set that requires a single, exclusive toggled item.
+ *
+ * @class Radio
+ * @extends GOWN.ToggleButton
+ * @memberof GOWN
+ * @constructor
+ * @param [theme] theme for the radio button {GOWN.Theme}
+ * @param [skinName=Radio.SKIN_NAME] name of the radio button skin {String}
+ */
 function Radio(theme, skinName) {
     skinName = skinName || Radio.SKIN_NAME;
     ToggleButton.call(this, theme, skinName);
@@ -17,13 +19,19 @@ Radio.prototype = Object.create( ToggleButton.prototype );
 Radio.prototype.constructor = Radio;
 module.exports = Radio;
 
-// name of skin that will be applied
+/**
+ * Default radio button skin name
+ *
+ * @static
+ * @final
+ * @type String
+ */
 Radio.SKIN_NAME = 'radio';
 
 /**
- * Create/Update the label of the button.
+ * Set the toggle group and add this radio button to it
  *
- * @property label
+ * @name GOWN.Radio#label
  * @type String
  */
 Object.defineProperty(Radio.prototype, 'toggleGroup', {
