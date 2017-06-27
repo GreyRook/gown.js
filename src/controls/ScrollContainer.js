00ddaa3763281fcc0ac15006286c23a6f8ef2b67
vdbongard <daniel@vdbongard.com>
2017-06-27 13:43:28 +0200

    Fix: ScrollContainer and ScrollThumb documentation

--- src/controls/ScrollContainer.js
+++ src/controls/ScrollContainer.js
@@ -2,10 +2,13 @@ var Scroller = require('./Scroller');
 //var ScrollBar = require('./ScrollBar');
 
 /**
+ * ScrollContainer (not implemented yet)
+ *
  * @class ScrollContainer
  * @extends GOWN.Scroller
  * @memberof GOWN
  * @constructor
+ * @param [theme] theme for the scroll container {GOWN.Theme}
  */
 function ScrollContainer(theme) {
     Scroller.call(this, theme);
