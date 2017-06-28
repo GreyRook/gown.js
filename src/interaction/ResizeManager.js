2668e69d2112e2213fd527b28f663a58e670471f
vdbongard <daniel@vdbongard.com>
2017-06-28 15:54:44 +0200

    Fix: ResizeManager documentation

--- src/interaction/ResizeManager.js
+++ src/interaction/ResizeManager.js
@@ -9,15 +9,14 @@ var EventEmitter = require('eventemitter3');
  * @class
  * @extends EventEmitter
  * @memberof GOWN.interaction
- * @param renderer {PIXI.CanvasRenderer|PIXI.WebGLRenderer} A reference to the current renderer
+ * @param renderer A reference to the current renderer {PIXI.CanvasRenderer|PIXI.WebGLRenderer}
  * @param [options] {object}
- * @param [options.autoPreventDefault=true] {boolean} Should the manager automatically prevent default browser actions.
+ * @param [options.autoPreventDefault=true] Should the manager automatically prevent default browser actions. {boolean}
+ * @param [options.fullscreen=false] Should we use the whole browser width/height (window.innerHeight/Width). {boolean}
  */
-
  // TODO: orientation change for cordova and cocoon ('orientationchange' and Cocoon.Device.getOrientation())
  // TODO: take a look at phaser ScaleManager - see https://github.com/photonstorm/phaser/blob/v2.4.2/src/core/ScaleManager.js
  // TODO: check single Canvas-DOM-Element, not only whole window (see https://github.com/marcj/css-element-queries )
-
 function ResizeManager(renderer, options) {
     EventEmitter.call(this);
 
@@ -26,36 +25,51 @@ function ResizeManager(renderer, options) {
     /**
      * The renderer this interaction manager works for.
      *
-     * @member {PIXI.SystemRenderer}
+     * @type PIXI.SystemRenderer
      */
     this.renderer = renderer;
 
     /**
      * Should default browser actions automatically be prevented.
      *
-     * @member {boolean}
+     * @type bool
      * @default true
      */
     this.autoPreventDefault = options.autoPreventDefault !== undefined ? options.autoPreventDefault : true;
 
     /**
-     * should we use the whole browser width/height (window.innerHeight/Width)
+     * Should we use the whole browser width/height (window.innerHeight/Width)
      *
-     * @member {boolean}
+     * @type bool
      * @default false
      */
     this.fullscreen = options.fullscreen || false;
 
+    /**
+     * TODO
+     */
     this.element = null;
 
+    /**
+     * Time to wait after every resize event to prevent flickering
+     *
+     * @type Number
+     * @default 0
+     */
     this.resizeTimeout = 0;
 
+    /**
+     * Should the resize manager wait after every resize event
+     *
+     * @type bool
+     * @default true
+     */
     this.useResizeDoneTimeout = options.useResizeDoneTimeout !== undefined ? options.useResizeDoneTimeout : true;
 
     /**
      * An event data object to handle all the event tracking/dispatching
      *
-     * @member {object}
+     * @type Object
      */
     this.eventData = {
         stopped: false,
@@ -78,8 +92,12 @@ ResizeManager.prototype.constructor = ResizeManager;
 module.exports = ResizeManager;
 
 /**
- * time after the resize when we will update
+ * The waiting time after the resize event before updating
  * (prevent the canvas from flickering when resizing)
+ *
+ * @static
+ * @final
+ * @type String
  */
 ResizeManager.RESIZE_DONE_TIMEOUT = 100;
 
@@ -106,7 +124,7 @@ ResizeManager.prototype.removeEvents = function () {
 /**
  * Is called when the application/browser window gets resized
  *
- * @param event {Event} The DOM event of a key being pressed down
+ * @param _event The DOM event {Event}
  * @private
  */
 ResizeManager.prototype.onResize = function (_event) {
@@ -134,11 +152,10 @@ ResizeManager.prototype.onResize = function (_event) {
 
 };
 
-
 /**
- * Handle original resize event and forward it
+ * Handle the original resize event and forward it
  *
- * @param event {Event} The DOM event
+ * @param event The DOM event {Event}
  * @private
  */
 ResizeManager.prototype._resizeEvent = function(event) {
@@ -154,9 +171,9 @@ ResizeManager.prototype._resizeEvent = function(event) {
     this.emit('resize', this.eventData);
 };
 
-
 /**
  * Grabs the size from the browser window
+ *
  * @private
  */
 ResizeManager.prototype.getSizeData = function (event) {
@@ -173,6 +190,12 @@ ResizeManager.prototype.getSizeData = function (event) {
     return eventData;
 };
 
+/**
+ * Dispatch a resize event for a given display object
+ *
+ * @param displayObject {PIXI.DisplayObject}
+ * @private
+ */
 ResizeManager.prototype.processResize = function(displayObject) {
     this.dispatchEvent( displayObject, 'resize', this.eventData );
 };
@@ -180,34 +203,31 @@ ResizeManager.prototype.processResize = function(displayObject) {
 /**
  * Dispatches an event on the display object that has resizable set to true
  *
- * @param displayObject {PIXI.Container|PIXI.Sprite|PIXI.extras.TilingSprite} the display object in question
- * @param eventString {string} the name of the event (e.g, resize or orientation)
- * @param eventData {object} the event data object
+ * @param displayObject The display object in question {PIXI.Container|PIXI.Sprite|PIXI.extras.TilingSprite}
+ * @param eventString The name of the event (e.g, resize or orientation) {string}
+ * @param eventData The event data object {object}
  * @private
  */
-ResizeManager.prototype.dispatchEvent = function ( displayObject, eventString, eventData )
-{
-    if(!eventData.stopped)
-    {
+ResizeManager.prototype.dispatchEvent = function (displayObject, eventString, eventData) {
+    if (!eventData.stopped) {
         eventData.target = displayObject;
         eventData.type = eventString;
 
-        displayObject.emit( eventString, eventData );
+        displayObject.emit(eventString, eventData);
 
-        if( displayObject[eventString] )
-        {
-            displayObject[eventString]( eventData );
+        if (displayObject[eventString]) {
+            displayObject[eventString](eventData);
         }
     }
 };
 
-
 /**
- * traverse through the scene graph to call given function on all displayObjects
+ * Traverse through the scene graph to call given function on all displayObjects
  * that are resizable
  *
- * @param displayObject {PIXI.Container|PIXI.Sprite|PIXI.extras.TilingSprite} the displayObject that will be resized (recurcsivly crawls its children)
- * @param [func] {Function} the function that will be called on each resizable object. The displayObject will be passed to the function
+ * @param displayObject the displayObject that will be resized (recursivly crawls its children)
+ * {PIXI.Container|PIXI.Sprite|PIXI.extras.TilingSprite}
+ * @param [func] the function that will be called on each resizable object. The displayObject will be passed to the function {Function}
  */
 ResizeManager.prototype.processInteractive = function (displayObject, func)
 {
@@ -233,6 +253,9 @@ ResizeManager.prototype.processInteractive = function (displayObject, func)
     }
 };
 
+/**
+ * Remove events and listener etc.
+ */
 ResizeManager.prototype.destroy = function(){
     this.removeEvents();
     this.removeAllListeners();
